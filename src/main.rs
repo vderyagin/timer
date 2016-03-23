8095b27769f8b6bd2f2a836b2b19fea8327bc38c
Victor Deryagin <vderyagin@gmail.com>
2016-03-23 14:25:14 +0200

    Rename a variable

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::time::Duration;
 use timer::timer::Timer;
 
 fn main() {
-  let tmr = Timer::from_args();
+  let timer = Timer::from_args();
   let check_interval = Duration::from_secs(60);
 
   loop {
-    tmr.tick();
+    timer.tick();
     thread::sleep(check_interval);
   };
 }
