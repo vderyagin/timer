612f4e5891e61f073e3c39198cd7f64d7a7902a3
Victor Deryagin <vderyagin@gmail.com>
2021-06-07 01:59:41 +0300

    format code

--- src/beeper.rs
+++ src/beeper.rs
@@ -1,9 +1,11 @@
-use rodio::{OutputStream, Sink};
 use rodio::source::{SineWave, Source};
+use rodio::{OutputStream, Sink};
 use std::time::Duration;
 
 pub fn beep() {
-    let source = SineWave::new(440).take_duration(Duration::from_secs_f32(0.4)).amplify(0.50);
+    let source = SineWave::new(440)
+        .take_duration(Duration::from_secs_f32(0.4))
+        .amplify(0.50);
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
 
