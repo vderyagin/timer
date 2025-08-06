ed3c9f6af42d466a757229a740d8102fef01e139
Victor Deryagin <vderyagin@gmail.com>
2025-08-06 18:48:12 +0300

    extract few consts

--- src/beeper.rs
+++ src/beeper.rs
@@ -2,9 +2,12 @@ use rodio::source::{SineWave, Source};
 use rodio::{OutputStreamBuilder, Sink};
 use std::time::Duration;
 
+const BEEP_FREQUENCY: f32 = 440.0;
+const BEEP_DURATION: f32 = 0.4;
+
 pub fn beep() {
-    let source = SineWave::new(440.0)
-        .take_duration(Duration::from_secs_f32(0.4))
+    let source = SineWave::new(BEEP_FREQUENCY)
+        .take_duration(Duration::from_secs_f32(BEEP_DURATION))
         .amplify(0.50);
     let stream_handle = OutputStreamBuilder::open_default_stream().unwrap();
     let sink = Sink::connect_new(&stream_handle.mixer());
